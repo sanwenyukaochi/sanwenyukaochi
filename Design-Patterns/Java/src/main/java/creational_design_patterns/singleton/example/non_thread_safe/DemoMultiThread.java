66e326ca8bf58bebb64530e5c524555ca0ea2ca4
sanwenyukaochi <sanwenyukaochi@outlook.com>
2026-09-12 13:26:45 +0800

    feat(并发): 添加多线程与单例演示类
    - 新增 DemoMultiThread.java：演示多线程并发执行场景
    - 新增 DemoSingleThread.java：提供单线程执行基准对照
    - 新增 Singleton.java：实现单例模式示例
    - 便于对比单线程与多线程行为差异
    
    Refs: 并发演示, 单例模式

---
+++ Design-Patterns/Java/src/main/java/creational_design_patterns/singleton/example/non_thread_safe/DemoMultiThread.java
@@ -0,0 +1,32 @@
+package creational_design_patterns.singleton.example.non_thread_safe;
+
+public class DemoMultiThread {
+    public static void main(String[] args) {
+        IO.println("""
+                If you see the same value, then singleton was reused (woohoo!)
+                If you see different values, then 2 singletons were created (ugh!!)
+                
+                RESULT:
+                """);
+        Thread threadFoo = new Thread(new ThreadFoo());
+        Thread threadBar = new Thread(new ThreadBar());
+        threadFoo.start();
+        threadBar.start();
+    }
+
+    static class ThreadFoo implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("FOO");
+            IO.println(singleton.value);
+        }
+    }
+
+    static class ThreadBar implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("BAR");
+            IO.println(singleton.value);
+        }
+    }
+}
